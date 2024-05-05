076073235d0f4990a52d1b04b8421f1b525821e8
Jihan <jihanaurelia2004@gmail.com>
2024-05-05 11:16:40 +0700

    testing branch

--- src/main/java/Plants/Plants.java
+++ src/main/java/Plants/Plants.java
@@ -84,4 +84,8 @@ public abstract class Plants {
     public void setTimeCreated(int timeCreated){
         this.timeCreated = timeCreated;
     }
+
+    public void test(){
+        System.out.println(name);
+    }
 }
