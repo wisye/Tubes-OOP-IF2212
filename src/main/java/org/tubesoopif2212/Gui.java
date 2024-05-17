e294eb5f17ac3a2dfe8c46bb102ab7eef513381a
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-05-17 18:45:16 +0700

    GUI

--- src/main/java/org/tubesoopif2212/Gui.java
+++ src/main/java/org/tubesoopif2212/Gui.java
@@ -2,33 +2,27 @@ package org.tubesoopif2212;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class Gui extends Application {
-    private static final int DISPLAY_WIDTH = 1280;
-    private static final int DISPLAY_HEIGHT = 720;
-    private static final int TILE_WIDTH_AMOUNT = 11;
-    private static final int TILE_HEIGHT_AMOUNT = 7;
-    private static final int TILE_WIDTH = DISPLAY_WIDTH / TILE_WIDTH_AMOUNT;
-    private static final int TILE_HEIGHT = DISPLAY_HEIGHT / TILE_HEIGHT_AMOUNT;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), DISPLAY_WIDTH, DISPLAY_HEIGHT);
-        stage.setTitle("Guardians of Flora Versus the Legions of Necrosis!");
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style.css")).toExternalForm());
         stage.setScene(scene);
+        stage.setTitle("Lalapan vs Michael");
+        stage.setWidth(800);
+        stage.setHeight(600);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
-    }
-
-    public void mainMenu(){
-        
+        launch(args);
     }
 }
