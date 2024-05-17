6903242b5259e999bbe761607e53355ac4442f14
Jihan <jihanaurelia2004@gmail.com>
2024-05-17 18:57:15 +0700

    plantList + zombieList

--- src/main/java/org/tubesoopif2212/gameLoop.java
+++ src/main/java/org/tubesoopif2212/gameLoop.java
@@ -17,18 +17,53 @@ public class gameLoop {
             Scanner scanner = new Scanner(System.in);
             // Thread gameThread = null;
             try {
-                menu();
+                menu(scanner);
                 choice = Integer.parseInt(scanner.nextLine());
                 if (choice == 1) {
                     startGame(scanner);
                 } else if (choice == 2) {
                     // help();
                 } else if (choice == 3) {
-                    // plantsList(scanner);
+                    int masukkan = -1;
+                    try{
+                        do {
+                            System.out.println("\n1. Sunflower");
+                            System.out.println("2. Peashooter");
+                            System.out.println("3. Wall-Nut");
+                            System.out.println("4. SnowPea");
+                            System.out.println("5. Squash");
+                            System.out.println("6. Lilypad");
+                            System.out.println("7. Nahida");
+                            System.out.println("8. Ceres Fauna");
+                            System.out.println("9. Cannabis");
+                            System.out.println("10. Planterra\n");
+                            System.out.print("Pilih tanaman (masukkan 0 jika tidak ada) : ");
+                            masukkan = Integer.parseInt(scanner.nextLine());
+                            System.out.println("");
+                            plantLists(masukkan);
+                        } while(masukkan != 0);
+                    } catch(Exception e){}
                 } else if (choice == 4) {
-                    // zombiesList(scanner);
+                    int masukkan = -1;
+                    try{
+                        do {
+                            System.out.println("\n1. Normal");
+                            System.out.println("2. Conehead");
+                            System.out.println("3. Pole Vaulting");
+                            System.out.println("4. Buckethead");
+                            System.out.println("5. Ducky Tube");
+                            System.out.println("6. Dolphin Rider");
+                            System.out.println("7. Entore Zom 100 Cast");
+                            System.out.println("8. Kureiji ollie");
+                            System.out.println("9. Qiqi");
+                            System.out.println("10. ShrekButZombie\n");
+                            System.out.print("Pilih zombie (masukkan 0 jika tidak ada) : ");
+                            masukkan = Integer.parseInt(scanner.nextLine());
+                            System.out.println("");
+                            plantLists(masukkan);
+                        } while(masukkan != 0);
+                    } catch(Exception e){}
                 } else if (choice == 5) {
-                    scanner.close();
                     System.out.println("Byee");
                     break;
                 } else {
@@ -40,7 +75,7 @@ public class gameLoop {
         }
     }
 
-    public static void menu() {
+    public static void menu(Scanner scanner) {
         System.out.println("1. Start\n" +
                 "2. Help\n" +
                 "3. Plants List\n" +
@@ -76,9 +111,9 @@ public class gameLoop {
         // Thread for game loop
         Thread gameThread = new Thread(() -> {
             int lastSunUpdate = 0;
-            // try{
-            // map.plant(5, 5, deck.create(0, seconds));
-            // } catch (Exception e){System.out.println(e.getMessage());}
+            try{
+            map.plant(5, 5, deck.create(0, seconds));
+            } catch (Exception e){System.out.println(e.getMessage());}
             // map.addZombie(0, new EntireZom100Cast(seconds));
             while (!gameOver && seconds < 200) {
                 try {
@@ -120,8 +155,8 @@ public class gameLoop {
                     seconds++;
                     // System.out.println(Sun.getAmount());
                     // System.out.println(Sun.getAmount());
-                    // map.printMap();
-                    // System.out.println();
+                    map.printMap();
+                    System.out.println();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -145,31 +180,28 @@ public class gameLoop {
             while (!gameOver) {
                 System.out.println(
                     "\n" +
-                    "Time: " + seconds + "\n" +
                     "<1 x y plants(index)> Plant tanaman di koordinat map\n" +
                     "<2 x y> Dig tanaman di koordinat map\n" +
-                    "<3> Print map\n" +
-                    "Sun: " + Sun.getAmount() + "\n" +
+                    Sun.getAmount() + "\n" +
                     deck.toString() + "\n"
                 );
                 int input = scanner.nextInt();
                 try{
                     if(input == 1){
                         map.plant(scanner.nextInt(), scanner.nextInt() - 1, deck.create(scanner.nextInt() - 1, seconds));
+                        map.printMap();
                     }
                     else if(input == 2){
                         map.dig(scanner.nextInt(), scanner.nextInt() - 1);
-                    }
-                    else if(input == 3){
+                        map.printMap();
                     }
                     else{
                         throw new Exception("Invalid input");
                     }
                 } catch(Exception e) {
                     System.out.println(e.getMessage());
-                } finally {
-                    map.printMap();
                 }
+
             }
         }).start();
 
@@ -236,6 +268,148 @@ public class gameLoop {
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    public static void help(){
+        System.out.println("Berikut adalah langkah dalam melakukan permainan:");
+        System.out.println("1. Pilih tanaman apa yang ingin kalian tanam, pastikan ada sunflower");
+    }
+
+    public static void plantLists(int idx){
+        // Nama kolom
+        String[] columnNames = {"Atribut\t", "Nilai"};
+        
+        String[][] data = null;
+
+        switch(idx){
+            case 1:
+                data = new String[][]{
+                    {"Name\t\t: ", "Sunflower"},
+                    {"Cost\t\t: ", "50"},
+                    {"Health\t\t: ", "100"},
+                    {"Attack Damage\t: ", "0"},
+                    {"Attack Speed\t: ", "0"},
+                    {"Range\t\t: ", "0"},
+                    {"Cooldown\t: ", "10"}
+                };
+                break;
+            case 2:
+                data = new String[][]{
+                    {"Name\t\t: ", "Peashooter"},
+                    {"Cost\t\t: ", "100"},
+                    {"Health\t\t: ", "100"},
+                    {"Attack Damage\t: ", "25"},
+                    {"Attack Speed\t: ", "4"},
+                    {"Range\t\t: ", "-1"},
+                    {"Cooldown\t: ", "10"}
+                };
+                break;
+            case 3:
+                data = new String[][]{
+                    {"Name\t\t: ", "Wall nut"},
+                    {"Cost\t\t: ", "50"},
+                    {"Health\t\t: ", "1000"},
+                    {"Attack Damage\t: ", "0"},
+                    {"Attack Speed\t: ", "0"},
+                    {"Range\t\t: ", "0"},
+                    {"Cooldown\t: ", "10"}
+                };
+                break;
+            case 4:
+                data = new String[][]{
+                    {"Name\t\t: ", "Snow Pea"},
+                    {"Cost\t\t: ", "175"},
+                    {"Health\t\t: ", "100"},
+                    {"Attack Damage\t: ", "25"},
+                    {"Attack Speed\t: ", "4"},
+                    {"Range\t\t: ", "-1"},
+                    {"Cooldown\t: ", "10"}
+                };
+                break;                
+            case 5:
+                data = new String[][]{
+                    {"Name\t\t: ", "Squash"},
+                    {"Cost\t\t: ", "50"},
+                    {"Health\t\t: ", "100"},
+                    {"Attack Damage\t: ", "5000"},
+                    {"Attack Speed\t: ", "0"},
+                    {"Range\t\t: ", "1"},
+                    {"Cooldown\t: ", "20"}
+                };
+                break;
+            case 6:
+                data = new String[][]{
+                    {"Name\t\t: ", "Lilypad"},
+                    {"Cost\t\t: ", "25"},
+                    {"Health\t\t: ", "100"},
+                    {"Attack Damage\t: ", "0"},
+                    {"Attack Speed\t: ", "0"},
+                    {"Range\t\t: ", "0"},
+                    {"Cooldown\t: ", "10"}
+                };
+                break;
+            case 7:
+                data = new String[][]{
+                    {"Name\t\t: ", "Nahida"},
+                    {"Cost\t\t: ", "-"},
+                    {"Health\t\t: ", "-"},
+                    {"Attack Damage\t: ", "-"},
+                    {"Attack Speed\t: ", "-"},
+                    {"Range\t\t: ", "-"},
+                    {"Cooldown\t: ", "-"}
+                };
+                break;
+            case 8:
+                data = new String[][]{
+                    {"Name\t\t: ", "Ceres Fauna"},
+                    {"Cost\t\t: ", "-"},
+                    {"Health\t\t: ", "-"},
+                    {"Attack Damage\t: ", "-"},
+                    {"Attack Speed\t: ", "-"},
+                    {"Range\t\t: ", "-"},
+                    {"Cooldown\t: ", "-"}
+                };
+                break;
+            case 9:
+                data = new String[][]{
+                    {"Name\t\t: ", "Cannabis"},
+                    {"Cost\t\t: ", "-"},
+                    {"Health\t\t: ", "-"},
+                    {"Attack Damage\t: ", "-"},
+                    {"Attack Speed\t: ", "-"},
+                    {"Range\t\t: ", "-"},
+                    {"Cooldown\t: ", "-"}
+                };
+                break;
+            case 10:
+                data = new String[][]{
+                    {"Name\t\t: ", "Planterra"},
+                    {"Cost\t\t: ", "-"},
+                    {"Health\t\t: ", "-"},
+                    {"Attack Damage\t: ", "-"},
+                    {"Attack Speed\t: ", "-"},
+                    {"Range\t\t: ", "-"},
+                    {"Cooldown\t: ", "-"}
+                };
+                break;
+            default:
+                break;
+        }
+
+
+        // Menampilkan header kolom
+        for (String columnName : columnNames) {
+            System.out.print(columnName + "\t");
+        }
+        System.out.println();
+
+        // Menampilkan data
+        for (String[] row : data) {
+            for (String cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println();
         }
     }
 }
